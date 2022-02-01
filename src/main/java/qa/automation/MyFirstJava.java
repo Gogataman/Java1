3d699c926eabcaa0942e037a41fd9b134cf068f5
Georgi Marinov <gogata2003@gmail.com>
2022-02-01 20:23:22 +0200

    Domashno1

--- src/main/java/qa/automation/MyFirstJava.java
+++ src/main/java/qa/automation/MyFirstJava.java
@@ -1,7 +1,22 @@
 package qa.automation;
 
+import java.util.Scanner;
+
 public class MyFirstJava {
 
+    public static void main(String[] args) {
 
+        System.out.println("Enter your name: ");
 
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+
+        System.out.println("Enter your hobby: ");
+
+        String hobby = scanner.nextLine();
+
+        System.out.println("Your name is: " + name +
+                "\nYour hobby is: " + hobby);
+    }
 }
